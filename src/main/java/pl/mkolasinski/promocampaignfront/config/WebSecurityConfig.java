7bfe8c4080c12c491939ff1bdc902c25e0f7878e
Kolachow <kolas3000@gmail.com>
2019-04-07 15:27:15 +0200

    Added info about current user

--- src/main/java/pl/mkolasinski/promocampaignfront/config/WebSecurityConfig.java
+++ src/main/java/pl/mkolasinski/promocampaignfront/config/WebSecurityConfig.java
@@ -7,11 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import pl.mkolasinski.promocampaignfront.security.LoginUserDetailsService;
 
 @Configuration
@@ -49,17 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .logoutSuccessUrl("/logout")
                 .permitAll();
     }
-//    @Bean
-//    @Override
-//    public UserDetailsService userDetailsService() {
-//        UserDetails user =
-//                User.withDefaultPasswordEncoder()
-//                        .username("user")
-//                        .password("password")
-//                        .roles("USER")
-//                        .build();
-//        return new InMemoryUserDetailsManager(user);
-//    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
--- src/main/java/pl/mkolasinski/promocampaignfront/controller/CampaignController.java
+++ src/main/java/pl/mkolasinski/promocampaignfront/controller/CampaignController.java
@@ -11,7 +11,6 @@ import pl.mkolasinski.promocampaignfront.config.Config;
 import pl.mkolasinski.promocampaignfront.model.CampaignDto;
 import pl.mkolasinski.promocampaignfront.model.CustomerDto;
 
-import java.security.Principal;
 import java.util.List;
 import java.util.Objects;
 
@@ -49,51 +48,60 @@ public class CampaignController {
     @GetMapping("/campaigns/all")
     public ModelAndView allCampaigns() {
         RestTemplate restTemplate = new RestTemplate();
+        String userEmail = getUserName(restTemplate);
 
         ResponseEntity<List> result = restTemplate.getForEntity(config.getBaseUrl() + "/campaigns", List.class);
         return new ModelAndView("campaigns-list")
-                .addObject("campaigns", result.getBody());
+                .addObject("campaigns", result.getBody())
+                .addObject("userEmail", userEmail);
     }
 
     @GetMapping("/campaigns/all/my")
     public ModelAndView customerCampaigns() {
         RestTemplate restTemplate = new RestTemplate();
         String brand = getBrand(restTemplate);
+        String userEmail = getUserName(restTemplate);
 
         ResponseEntity<List> result = restTemplate.getForEntity(config.getBaseUrl()+"/campaigns/all/".concat(brand), List.class);
         return new ModelAndView("my-campaigns")
-                .addObject("campaigns", result.getBody());
+                .addObject("campaigns", result.getBody())
+                .addObject("userEmail", userEmail);
     }
 
     @GetMapping("/campaigns/current")
     public ModelAndView brandCampaigns() {
         RestTemplate restTemplate = new RestTemplate();
         String brand = getBrand(restTemplate);
+        String userEmail = getUserName(restTemplate);
 
         ResponseEntity<List> result = restTemplate.getForEntity(config.getBaseUrl() + "/campaigns/current/".concat(brand), List.class);
         return new ModelAndView("campaigns-list")
-                .addObject("campaigns", result.getBody());
+                .addObject("campaigns", result.getBody())
+                .addObject("userEmail", userEmail);
     }
 
     @GetMapping("/campaigns/ended")
     public ModelAndView endedCampaigns() {
         RestTemplate restTemplate = new RestTemplate();
         String brand = getBrand(restTemplate);
+        String userEmail = getUserName(restTemplate);
 
         ResponseEntity<List> result = restTemplate.getForEntity(config.getBaseUrl() + "/campaigns/ended/".concat(brand), List.class);
         return new ModelAndView("campaigns-list")
-                .addObject("campaigns", result.getBody());
+                .addObject("campaigns", result.getBody())
+                .addObject("userEmail", userEmail);
     }
-
 
     @GetMapping("/campaigns/future")
     public ModelAndView futureCampaigns() {
         RestTemplate restTemplate = new RestTemplate();
         String brand = getBrand(restTemplate);
+        String userEmail = getUserName(restTemplate);
 
         ResponseEntity<List> result = restTemplate.getForEntity(config.getBaseUrl() + "/campaigns/future/".concat(brand), List.class);
         return new ModelAndView("campaigns-list")
-                .addObject("campaigns", result.getBody());
+                .addObject("campaigns", result.getBody())
+                .addObject("userEmail", userEmail);
     }
 
     private String getBrand(RestTemplate restTemplate) {
@@ -101,5 +109,12 @@ public class CampaignController {
         String customerEmail = customer.getUsername();
         ResponseEntity<CustomerDto> customerResponse = restTemplate.getForEntity(config.getBaseUrl() + "/customers/" + customerEmail, CustomerDto.class);
         return Objects.requireNonNull(customerResponse.getBody()).getCompany();
+    }
+
+    private String getUserName(RestTemplate restTemplate) {
+        User customer = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String customerEmail = customer.getUsername();
+
+        return customerEmail;
     }
 }
